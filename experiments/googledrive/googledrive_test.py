0b9c26c1ee9523f06a213bdc20c88517c29cff49
Pier-Luc Brault <plbrault@gmail.com>
2014-09-21 15:21:47 -0400

    upload file Hello.txt

--- experiments/googledrive/googledrive_test.py
+++ experiments/googledrive/googledrive_test.py
@@ -1,6 +1,13 @@
 # Important: client_secrets.json must be present in the same directory as this Python file
 
 from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication
+
+drive = GoogleDrive(gauth)
+
+file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'
+file1.SetContentString('Hello World!') # Set content of the file from given string
+file1.Upload()
