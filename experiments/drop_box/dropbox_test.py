8a867b7966a0a7ba87ed4305157d18d0668bc9e6
Pier-Luc Brault <plbrault@gmail.com>
2014-09-21 11:44:38 -0400

    dropbox authentication

--- experiments/drop_box/dropbox_test.py
+++ experiments/drop_box/dropbox_test.py
@@ -2,3 +2,23 @@ from drop_box.settings import Settings
 from dropbox.client import DropboxOAuth2FlowNoRedirect
 
 flow = DropboxOAuth2FlowNoRedirect(Settings.app_key, Settings.app_secret)
+
+
+authorize_url = flow.start()
+
+authenticated = False
+while not authenticated:
+    print('1. Go to: ' + authorize_url)
+    print('2. Click "Allow" (you might have to log in first)')
+    print('3. Copy the authorization code.')
+    print('4. Enter the authorization code :')
+    
+    code = input()
+    print()
+    
+    try:        
+        access_token, user_id = flow.finish(code)
+        print("Authentication succeeded")
+        authenticated = True
+    except:
+        print("Authentication failed. Try again :")
