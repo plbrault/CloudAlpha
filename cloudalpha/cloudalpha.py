cdc64090311d94f6ae809fe3b56066ea7b454a72
Pier-Luc Brault <plbrault@gmail.com>
2014-09-24 15:20:28 -0400

    created main that associates a commandline manager with a dummy account

--- cloudalpha/cloudalpha.py
+++ cloudalpha/cloudalpha.py
@@ -1,2 +1,12 @@
+from services.dummy.account import DummyAccount
+from managers.commandline.manager import CommandLineManager
+
 if __name__ == '__main__':
-    pass
+    
+    account = DummyAccount("dummy1")
+    manager = CommandLineManager("commandline1")
+    
+    manager.file_system = account.file_system
+    
+    account.authenticate()
+    manager.run()
