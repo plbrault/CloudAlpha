e3c9c76eb3c85e9adddd7fd35849e04848619f5e
Pier-Luc Brault <plbrault@gmail.com>
2014-10-30 16:04:38 -0400

    fixed error in get_size

--- cloudalpha/services/dropbox/file_system.py
+++ cloudalpha/services/dropbox/file_system.py
@@ -123,7 +123,7 @@ class DropBoxFileSystem(FileSystem):
                     raise InvalidTargetFileSystemError()
                 try:
                     dropbox_meta = self._client.metadata(path)
-                    return not dropbox_meta["size"]
+                    return dropbox_meta["size"]
                 except:
                     raise AccessFailedFileSystemError()
             else:
