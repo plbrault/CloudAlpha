1e5e964f331c5e21ebed3eea2e6834658cd2360c
Pier-Luc Brault <plbrault@gmail.com>
2014-11-15 14:34:50 -0500

    removed debug prints

--- cloudalpha/managers/ftp/ftp_server/adapted_dtp_handler.py
+++ cloudalpha/managers/ftp/ftp_server/adapted_dtp_handler.py
@@ -1,10 +1,4 @@
-from pyftpdlib.handlers import DTPHandler, _FileReadWriteError
-import socket
-import sys
-
-########## TEMP #####################
-from pyftpdlib.ioloop import AsyncChat
-######################################
+from pyftpdlib.handlers import DTPHandler
 
 class AdaptedDTPHandler(DTPHandler):
 
@@ -29,7 +23,6 @@ class AdaptedDTPHandler(DTPHandler):
             self._new_file_id = new_file_id
 
         def write(self, chunk):
-            print("file_obj.write", chunk)
             self._file_system_view.write_to_new_file(self._new_file_id, chunk)
 
         def close(self):
@@ -39,7 +32,6 @@ class AdaptedDTPHandler(DTPHandler):
 
     @property
     def file_obj(self):
-        print("file_obj")
         if self.file_path is None:
             return None
         else:
@@ -47,29 +39,21 @@ class AdaptedDTPHandler(DTPHandler):
 
     @file_obj.setter
     def file_obj(self, obj):
-        print("file_obj", obj)
-
         pass
 
     def __init__(self, sock, cmd_channel):
         super(AdaptedDTPHandler, self).__init__(sock, cmd_channel)
 
     def _posix_ascii_data_wrapper(self, chunk):
-        print("_posix_ascii_data_wrapper", chunk)
-
         return chunk
 
     def enable_receiving(self, type, cmd):
-        print("enable_receiving", type, cmd)
-
         self.file_path = self.cmd_channel.upload_path
         file_system_view = self.cmd_channel.fs.file_system_view
         self._new_file_id = file_system_view.create_new_file()
         super(AdaptedDTPHandler, self).enable_receiving(type, cmd)
 
     def close(self):
-        print("close")
-
         if not self._closed and self.file_obj != None:
             file_system_view = self.cmd_channel.fs.file_system_view
             if self.transfer_finished:
@@ -78,121 +62,3 @@ class AdaptedDTPHandler(DTPHandler):
                 file_system_view.flush_new_file(self._new_file_id)
         self.cmd_channel._on_dtp_close()
         super(AdaptedDTPHandler, self).close()
-
-
-
-
-
-
-    ###### TEMP ######
-
-    def __repr__(self):
-        print("__repr__")
-        return super(AdaptedDTPHandler, self).__repr__()
-
-    __str__ = __repr__
-
-    def _use_sendfile(self, producer):
-        print("_use_sendfile")
-        return super(AdaptedDTPHandler, self)._use_sendfile(producer)
-
-    def push(self, data):
-        return super(AdaptedDTPHandler, self).push(data)
-
-    def push_with_producer(self, producer):
-        print("push_with_producer")
-
-        self._initialized = True
-        self.ioloop.modify(self._fileno, self.ioloop.WRITE)
-        if self._use_sendfile(producer):
-            print ("... if")
-            self._offset = producer.file.tell()
-            self._filefd = self.file_obj.fileno()
-            self.initiate_sendfile()
-            self.initiate_send = self.initiate_sendfile
-        else:
-            print("... else")
-            AsyncChat.push_with_producer(self, producer)
-
-    def close_when_done(self):
-        print("close_when_done")
-        return super(AdaptedDTPHandler, self).close_when_done()
-
-    def initiate_send(self):
-        print("initiate_send")
-        return super(AdaptedDTPHandler, self).initiate_send()
-
-    def initiate_sendfile(self):
-        print("initiate_sendfile")
-        return super(AdaptedDTPHandler, self).initiate_sendfile()
-
-    # --- utility methods
-
-    def get_transmitted_bytes(self):
-        print("get_transmitted_bytes")
-        return super(AdaptedDTPHandler, self).get_transmitted_bytes()
-
-    def get_elapsed_time(self):
-        print("get_elapsed_time")
-        return super(AdaptedDTPHandler, self).get_elapsed_time()
-
-    def transfer_in_progress(self):
-        print("transfer_in_progress")
-        return super(AdaptedDTPHandler, self).transfer_in_progress()
-
-    # --- connection
-
-    def send(self, data):
-        print("send")
-        return super(AdaptedDTPHandler, self).send(data)
-
-    def refill_buffer(self):
-        print("refill_buffer")
-        return super(AdaptedDTPHandler, self).refill_buffer()
-
-    def handle_read(self):
-        print("handle_read")
-
-        try:
-            chunk = self.recv(self.ac_in_buffer_size)
-        except socket.error:
-            self.handle_error()
-        else:
-            self.tot_bytes_received += len(chunk)
-            if not chunk:
-                self.transfer_finished = True
-                # self.close()  # <-- asyncore.recv() already do that...
-                return
-            if self._data_wrapper is not None:
-                chunk = self._data_wrapper(chunk)
-            try:
-                self.file_obj.write(chunk)
-            except OSError:
-                err = sys.exc_info()[1]
-                raise _FileReadWriteError(err)
-
-
-    handle_read_event = handle_read  # small speedup
-
-    def readable(self):
-        print("readable")
-        return super(AdaptedDTPHandler, self).readable()
-
-    debug_writable = False
-    def writable(self):
-        if not self.debug_writable:
-            print("writable")
-            self.debug_writable = True
-        return super(AdaptedDTPHandler, self).writable()
-
-    def handle_timeout(self):
-        print("handle_timeout")
-        return super(AdaptedDTPHandler, self).handle_timeout()
-
-    def handle_error(self):
-        print("handle_error")
-        return super(AdaptedDTPHandler, self).handle_error()
-
-    def handle_close(self):
-        print("handle_close")
-        return super(AdaptedDTPHandler, self).handle_close()
--- cloudalpha/managers/ftp/ftp_server/file_system_adapter.py
+++ cloudalpha/managers/ftp/ftp_server/file_system_adapter.py
@@ -15,8 +15,6 @@ class FileSystemAdapter(AbstractedFS):
 
     @staticmethod
     def get_class(manager_unique_id, file_system_view):
-        print("get_class", file_system_view)
-
         """Create a new FileSystemAdapter subclass bound to the given FileSystemView instance."""
         FileSystemAdapter._next_class_id += 1
         return type("FileSystemAdapter_cls" + str(FileSystemAdapter._next_class_id), (FileSystemAdapter,),
@@ -24,64 +22,44 @@ class FileSystemAdapter(AbstractedFS):
 
     @property
     def cwd(self):
-        print("cwd")
-
         """Return the current working directory."""
         return self.file_system_view.working_dir
 
     @cwd.setter
     def cwd(self, path):
-        print("cwd", path)
-
         """Set the current working directory."""
         self.file_system_view.working_dir = path
 
     @property
     def root(self):
-        print("root")
-
         """Return the root path of the file system."""
         return "/"
 
     @root.setter
     def root(self, path):
-        print("root", path)
-
         """Override the root setter of the base class to make it effectless"""
         pass
 
     def ftpnorm(self, path):
-        print("ftpnorm", path)
-
         """Return the absolute path corresponding to the given relative path."""
         return self.file_system_view.get_abs_path(path.replace("\\", "/"))
 
     def ftp2fs(self, ftppath):
-        print("ftp2fs", ftppath)
-
         """Same as ftpnorm."""
         return self.ftpnorm(ftppath)
 
     def fs2ftp(self, fspath):
-        print("fs2ftp", fspath)
-
         """Return fspath as is."""
         return fspath
 
     def validpath(self, path):
-        print("validpath", path)
-
         """Return true if the given path starts with "/"."""
         return path.startswith("/")
 
     def open(self, filename, mode):
-        print("open", filename, mode)
-
         pass
 
     def chdir(self, path):
-        print("chdir", path)
-
         """Change the current directory."""
         try:
             self.file_system_view.working_dir = path
@@ -91,14 +69,10 @@ class FileSystemAdapter(AbstractedFS):
             raise NotADirectoryError
 
     def mkdir(self, path):
-        print("mkdir", path)
-
         """Create the specified directory."""
         self.file_system_view.make_dir(path)
 
     def listdir(self, path):
-        print("listdir", path)
-
         """List the content of a directory."""
         if path[-1] != "/":
             path += "/"
@@ -109,26 +83,18 @@ class FileSystemAdapter(AbstractedFS):
         return res
 
     def rmdir(self, path):
-        print("rmdir", path)
-
         """Remove the specified directory."""
         self.file_system_view.delete(path)
 
     def remove(self, path):
-        print("remove", path)
-
         """Remove the specified file."""
         self.file_system_view.delete(path)
 
     def rename(self, src, dst):
-        print("rename", src, dst)
-
         """Rename the specified src file to the dst filename."""
         self.file_system_view.move(src, dst)
 
     def chmod(self, path, mode):
-        print("chmod", path, mode)
-
         """Do nothing."""
         pass
 
@@ -150,8 +116,6 @@ class FileSystemAdapter(AbstractedFS):
         return meta
 
     def stat(self, path):
-        print("stat", path)
-
         """Emulate a stat() system call on the given path."""
         meta = self.metadata(path)
         mode = StatResult.Modes.FILE
@@ -160,67 +124,45 @@ class FileSystemAdapter(AbstractedFS):
         return StatResult(mode, meta.size, meta.accessed_datetime.timestamp(), meta.modified_datetime.timestamp(), meta.created_datetime.timestamp())
 
     def lstat(self, path):
-        print("lstat", path)
-
         """Same as stat."""
         return self.stat(path)
 
     def readlink(self, path):
-        print("readlink", path)
-
         """Return the given path as is."""
         return path
 
     def isfile(self, path):
-        print("isfile", path)
-
         """Return True if path is a file."""
         return not self.metadata(path).is_dir
 
     def islink(self, path):
-        print("islink", path)
-
         """Return False."""
         return False
 
     def isdir(self, path):
-        print("isdir", path)
-
         """Return True if path is a directory."""
         return self.metadata(path).is_dir
 
     def getsize(self, path):
-        print("getsize", path)
-
         """Return the size of the specified file in bytes."""
         return self.metadata(path).size
 
     def getmtime(self, path):
-        print("getmtime", path)
-
         """Return the last modified time of path as a number of seconds since the epoch."""
         return self.metadata(path).modified_datetime.timestamp()
 
     def realpath(self, path):
-        print("realpath", path)
-
         """Return the given path as is."""
         return path
 
     def lexists(self, path):
-        print("lexists", path)
-
         """Same as validpath."""
         return self.validpath(path)
 
     def get_user_by_uid(self, uid):
-        print("get_user_by_uid", uid)
-
         """Return the value "owner"."""
         return "owner"
 
     def get_group_by_gid(self, gid):
-        print("get_group_by_gid", gid)
-
         """Return the value "group"."""
         return "group"
