724a7bab688cfbb186e6e1a29f0d7efb7d352c1e
Pier-Luc Brault <plbrault@gmail.com>
2014-11-15 22:06:39 -0500

    working main (running multiple FTP servers at once does not work)

--- cloudalpha/cloudalpha.py
+++ cloudalpha/cloudalpha.py
@@ -4,15 +4,8 @@ from managers.commandline.manager import CommandLineManager
 from managers.ftp.manager import FTPManager
 
 if __name__ == '__main__':
-
-    dummyAccount = DummyAccount("dummy1")
-    dummyAccount.authenticate()
-
     dropboxAccount = DropBoxAccount("dropbox1")
     dropboxAccount.authenticate()
 
-    dummy_ftp_manager = FTPManager("ftp_dummy", dummyAccount.file_system.get_new_view(), 2121, "user", "12345")
-    dummy_ftp_manager.run()
-
-    dropbox_ftp_manager = FTPManager("ftp_dropbox", dropboxAccount.file_system.get_new_view(), 2122, "user", "12345")
+    dropbox_ftp_manager = FTPManager("ftp_dropbox", dropboxAccount.file_system.get_new_view(), 2121, "user", "12345")
     dropbox_ftp_manager.run()
