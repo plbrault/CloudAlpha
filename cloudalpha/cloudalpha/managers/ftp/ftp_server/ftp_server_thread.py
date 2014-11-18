ae8a396f8e896b5b3b0b7c0d2dfbf210d770fdf4
Pier-Luc Brault <plbrault@gmail.com>
2014-11-17 22:52:40 -0500

    added docstrings to FTPServerThread

--- cloudalpha/cloudalpha/managers/ftp/ftp_server/ftp_server_thread.py
+++ cloudalpha/cloudalpha/managers/ftp/ftp_server/ftp_server_thread.py
@@ -1,11 +1,15 @@
 from threading import Thread
 
 class FTPServerThread(Thread):
+    """A thread for running a FTPServer instance."""
+
     ftp_server = None
 
     def __init__(self, ftp_server):
+        """FTPServerThread initializer"""
         self.ftp_server = ftp_server
         super(FTPServerThread, self).__init__()
 
     def run(self):
+        """Start the thread and the FTP server."""
         self.ftp_server.serve_forever()
