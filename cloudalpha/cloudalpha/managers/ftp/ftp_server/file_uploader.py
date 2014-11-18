9a9b6047cb0f4d726e2a22bc53e8eed5ae87d7f0
Pier-Luc Brault <plbrault@gmail.com>
2014-11-17 22:50:41 -0500

    added docstrings to FileUploader

--- cloudalpha/cloudalpha/managers/ftp/ftp_server/file_uploader.py
+++ cloudalpha/cloudalpha/managers/ftp/ftp_server/file_uploader.py
@@ -1,4 +1,6 @@
 class FileUploader(object):
+    """This class handles the writing to a file of the file system
+    in order to store files transferred from the client to the server."""
 
     _file_path = None
     _new_file_id = None
@@ -8,15 +10,19 @@ class FileUploader(object):
 
     @property
     def name(self):
+        """Return the path of the file."""
         return self._file_path
 
     def __init__(self, file_system_view, file_path, new_file_id):
+        """FileUploader initializer"""
         self._file_system_view = file_system_view
         self._file_path = file_path
         self._new_file_id = new_file_id
 
     def write(self, chunk):
+        """Append a chunk of data to the end of the file."""
         self._file_system_view.write_to_new_file(self._new_file_id, chunk)
 
     def close(self):
+        """Set closed to True."""
         self.closed = True
