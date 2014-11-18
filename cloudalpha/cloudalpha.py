6fddc85d8178102d08c6a4d1d677e05dced5b2a7
Pier-Luc Brault <plbrault@gmail.com>
2014-11-17 23:25:27 -0500

    added docstring to main

--- cloudalpha/cloudalpha.py
+++ cloudalpha/cloudalpha.py
@@ -1,6 +1,7 @@
 from configurator.configurator import Configurator, ConfiguratorError
 
 if __name__ == '__main__':
+    """Generate the accounts and managers specified in config.xml, and launch them."""
     try:
         configurator = Configurator("config.xml")
         for account in configurator.get_accounts():
