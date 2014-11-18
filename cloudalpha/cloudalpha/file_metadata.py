172b4d5286c28a2ad9256cca762eb4bbbb43e342
Pier-Luc Brault <plbrault@gmail.com>
2014-11-17 23:14:39 -0500

    added docstrings to FileMetadata

--- cloudalpha/cloudalpha/file_metadata.py
+++ cloudalpha/cloudalpha/file_metadata.py
@@ -1,5 +1,5 @@
 class FileMetadata(object):
-    """Represent the metadata of a file."""
+    """Class representing the metadata of a file."""
 
     path = ""
     is_dir = False
@@ -10,12 +10,14 @@ class FileMetadata(object):
 
     @property
     def name(self):
+        """Return the short name of the file, excluding its location."""
         path = self.path
         if path[-1:] == "/":
             path = path[:-1]
         return path.rsplit("/", 1)[-1]
 
     def __init__(self, path="", is_dir=False, size=0, created_datetime=None, accessed_datetime=None, modified_datetime=None):
+        """FileMetadata initializer"""
         self.path = path
         self.is_dir = is_dir
         self.size = size
@@ -24,6 +26,7 @@ class FileMetadata(object):
         self.modified_datetime = modified_datetime
 
     def __str__(self):
+        """Return a string representing the object."""
         return ('FileMetadata(path="' + self.path + '", name="' + self.name + '", is_dir=' + str(self.is_dir) + ", size=" + str(self.size)
             + ", created_datetime=" + str(self.created_datetime) + ", accessed_datetime=" + str(self.accessed_datetime)
             + ", modified_datetime=" + str(self.modified_datetime) + ")")
