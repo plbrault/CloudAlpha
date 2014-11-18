ef856e367befef90f5a2e94b0b661af66cf107b1
Pier-Luc Brault <plbrault@gmail.com>
2014-11-17 22:29:12 -0500

    added docstrings to AdaptedDTPHandler methods

--- cloudalpha/cloudalpha/managers/ftp/ftp_server/adapted_dtp_handler.py
+++ cloudalpha/cloudalpha/managers/ftp/ftp_server/adapted_dtp_handler.py
@@ -13,6 +13,10 @@ class AdaptedDTPHandler(DTPHandler):
 
     @property
     def file_obj(self):
+        """Shadow the file_obj attribute of the baseclass.
+        If file_path is not set, return None. If file_path is
+        set, return the corresponding FileUploader object.
+        """
         if self.file_path is None:
             return None
         elif self._file_uploader is None:
@@ -21,21 +25,34 @@ class AdaptedDTPHandler(DTPHandler):
 
     @file_obj.setter
     def file_obj(self, obj):
+        """Dummy setter of the file_obj property, that shadows
+        the file_obj attribute of the baseclass. It has no effect.
+        """
         pass
 
     def __init__(self, sock, cmd_channel):
+        """AdaptedDTPHandler initializer"""
         super(AdaptedDTPHandler, self).__init__(sock, cmd_channel)
 
     def _posix_ascii_data_wrapper(self, chunk):
+        """Override the corresponding method of the baseclass.
+        Return chunk as is.
+        """
         return chunk
 
     def enable_receiving(self, type, cmd):
+        """This method is called by the FTP handler to initialize
+        the receiving of data from the client. 
+        """
         self.file_path = self.cmd_channel.upload_path
         file_system_view = self.cmd_channel.fs.file_system_view
         self._new_file_id = file_system_view.create_new_file()
         super(AdaptedDTPHandler, self).enable_receiving(type, cmd)
 
     def close(self):
+        """This method is called by the FTP handler when data
+        transmission is finished.
+        """
         if not self._closed and self.file_obj != None:
             file_system_view = self.cmd_channel.fs.file_system_view
             if self.transfer_finished:
